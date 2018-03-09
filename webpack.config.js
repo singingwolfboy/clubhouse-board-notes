58e8f80218f36a21f58b20a1a4a56b77ce8d3f32
David Baumgold <david@impraise.com>
2018-03-09 10:39:24 +0100

    simplify webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,24 @@
 var webpack = require("webpack"),
-    path = require("path"),
-    fileSystem = require("fs"),
-    env = require("./utils/env"),
-    CleanWebpackPlugin = require("clean-webpack-plugin"),
-    CopyWebpackPlugin = require("copy-webpack-plugin"),
-    HtmlWebpackPlugin = require("html-webpack-plugin"),
-    WriteFilePlugin = require("write-file-webpack-plugin");
+  path = require("path"),
+  fileSystem = require("fs"),
+  CleanWebpackPlugin = require("clean-webpack-plugin"),
+  CopyWebpackPlugin = require("copy-webpack-plugin"),
+  HtmlWebpackPlugin = require("html-webpack-plugin"),
+  WriteFilePlugin = require("write-file-webpack-plugin"),
+  ChromeExtensionReloader = require("webpack-chrome-extension-reloader");
 
-// load the secrets
-var alias = {};
-
-var secretsPath = path.join(__dirname, ("secrets." + env.NODE_ENV + ".js"));
-
-var fileExtensions = ["jpg", "jpeg", "png", "gif", "eot", "otf", "svg", "ttf", "woff", "woff2"];
-
-if (fileSystem.existsSync(secretsPath)) {
-  alias["secrets"] = secretsPath;
-}
+var fileExtensions = [
+  "jpg",
+  "jpeg",
+  "png",
+  "gif",
+  "eot",
+  "otf",
+  "svg",
+  "ttf",
+  "woff",
+  "woff2"
+];
 
 var options = {
   entry: {
@@ -30,9 +32,9 @@ var options = {
   module: {
     rules: [
       {
-        test: new RegExp('\.(' + fileExtensions.join('|') + ')$'),
-        loader: "file-loader?name=[name].[ext]",
-        exclude: /node_modules/
+        test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
+        exclude: /node_modules/,
+        use: "file-loader?name=[name].[ext]"
       },
       {
         test: /\.html$/,
@@ -41,7 +43,7 @@ var options = {
       },
       {
         test: /\.svg$/,
-        use: ["babel-loader", "react-svg-loader"],
+        use: ["babel-loader", "react-svg-loader"]
       },
       {
         test: /\.(js|jsx)$/,
@@ -51,33 +53,35 @@ var options = {
     ]
   },
   resolve: {
-    alias: alias,
-    extensions: fileExtensions.map(extension => ("." + extension)).concat([".jsx", ".js", ".css"])
+    extensions: fileExtensions
+      .map(extension => "." + extension)
+      .concat([".jsx", ".js", ".css"])
   },
   plugins: [
     // clean the build folder
     new CleanWebpackPlugin(["build"]),
     // expose and write the allowed env vars on the compiled bundle
     new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(env.NODE_ENV)
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     }),
-    new CopyWebpackPlugin([{
-      from: "src/manifest.json",
-      transform: function (content, path) {
-        // generates the manifest file using the package.json informations
-        return Buffer.from(JSON.stringify({
-          description: process.env.npm_package_description,
-          version: process.env.npm_package_version,
-          ...JSON.parse(content.toString())
-        }))
+    new CopyWebpackPlugin([
+      {
+        from: "src/manifest.json",
+        transform: function(content, path) {
+          // generates the manifest file using the package.json informations
+          return Buffer.from(
+            JSON.stringify({
+              description: process.env.npm_package_description,
+              version: process.env.npm_package_version,
+              ...JSON.parse(content.toString())
+            })
+          );
+        }
       }
-    }]),
-    new WriteFilePlugin()
+    ]),
+    new WriteFilePlugin(),
+    new ChromeExtensionReloader()
   ]
 };
-
-if (env.NODE_ENV === "development") {
-  options.devtool = "cheap-module-eval-source-map";
-}
 
 module.exports = options;
