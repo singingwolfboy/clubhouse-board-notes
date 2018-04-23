2ab0f2e5996ffe97a4853eab90982817c46ba76e
David Baumgold <david@impraise.com>
2018-04-23 12:01:28 +0200

    Check CLI mode in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ var webpack = require("webpack"),
   WriteFilePlugin = require("write-file-webpack-plugin"),
   ChromeExtensionReloader = require("webpack-chrome-extension-reloader");
 
-var fileExtensions = [
+const fileExtensions = [
   "jpg",
   "jpeg",
   "png",
@@ -19,71 +19,74 @@ var fileExtensions = [
   "woff2"
 ];
 
-var options = {
-  entry: {
-    background: path.join(__dirname, "src", "js", "background.js"),
-    foreground: path.join(__dirname, "src", "js", "foreground.js")
-  },
-  output: {
-    path: path.join(__dirname, "build"),
-    filename: "[name].bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        loader: "html-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: /\.svg$/,
-        use: ["babel-loader", "react-svg-loader"]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        loader: "babel-loader",
-        exclude: /node_modules/
-      },
-      {
-        test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
-        exclude: /node_modules/,
-        use: "file-loader?name=[name].[ext]"
-      }
-    ]
-  },
-  resolve: {
-    extensions: fileExtensions
-      .map(extension => "." + extension)
-      .concat([".jsx", ".js", ".css"])
-  },
-  plugins: [
-    // clean the build folder
-    new CleanWebpackPlugin(["build"]),
-    // expose and write the allowed env vars on the compiled bundle
-    new webpack.DefinePlugin({
-      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: "src/manifest.json",
-        transform: function(content, path) {
-          // generates the manifest file using the package.json informations
-          return Buffer.from(
-            JSON.stringify({
-              description: process.env.npm_package_description,
-              version: process.env.npm_package_version,
-              ...JSON.parse(content.toString())
-            })
-          );
+module.exports = (env, options) => {
+  const mode = options.mode || "production";
+  const config = {
+    entry: {
+      background: path.join(__dirname, "src", "js", "background.js"),
+      foreground: path.join(__dirname, "src", "js", "foreground.js")
+    },
+    output: {
+      path: path.join(__dirname, "build"),
+      filename: "[name].bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          loader: "html-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: /\.svg$/,
+          use: ["babel-loader", "react-svg-loader"]
+        },
+        {
+          test: /\.(js|jsx)$/,
+          loader: "babel-loader",
+          exclude: /node_modules/
+        },
+        {
+          test: new RegExp(".(" + fileExtensions.join("|") + ")$"),
+          exclude: /node_modules/,
+          use: "file-loader?name=[name].[ext]"
         }
-      }
-    ]),
-    new WriteFilePlugin()
-  ]
+      ]
+    },
+    resolve: {
+      extensions: fileExtensions
+        .map(extension => "." + extension)
+        .concat([".jsx", ".js", ".css"])
+    },
+    plugins: [
+      // clean the build folder
+      new CleanWebpackPlugin(["build"]),
+      // expose and write the allowed env vars on the compiled bundle
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(mode)
+      }),
+      new CopyWebpackPlugin([
+        {
+          from: "src/manifest.json",
+          transform: function(content, path) {
+            // generates the manifest file using the package.json informations
+            return Buffer.from(
+              JSON.stringify({
+                description: process.env.npm_package_description,
+                version: process.env.npm_package_version,
+                ...JSON.parse(content.toString())
+              })
+            );
+          }
+        }
+      ]),
+      new WriteFilePlugin()
+    ]
+  };
+
+  if (mode === "development") {
+    config.plugins.push(new ChromeExtensionReloader());
+  }
+
+  return config;
 };
-
-if (process.env.NODE_ENV === "development") {
-  options.plugins.push(new ChromeExtensionReloader());
-}
-
-module.exports = options;
